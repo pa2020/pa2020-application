216133a863c60d131be18fdf503023f003a62432
Desjardins Alexis <vizuar.gamer@gmail.com>
2020-06-29 14:18:25 +0200

    Change number of row to %age in Analyze Add Auth in the Request object

--- main/src/thread/satisfaction.py
+++ main/src/thread/satisfaction.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from datetime import date
+from datetime import datetime
 
 import main.src.learning.model as learn
 from main.src.api.request import Request
@@ -12,6 +12,7 @@ logger()
 
 
 def runAnalyze(word, reqId):
+    now = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))  # 2020-06-29T08:29:46.965000000
     req = Request()
     log.info('Requesting last tweets with : "' + word + '"')
     res = req.tweets(word)
@@ -19,13 +20,29 @@ def runAnalyze(word, reqId):
     log.info('Predicting sentiment of tweets')
     feels = learn.analyze(cleaned)
     stats_sentence, stats_ratio = learn.statistics(feels)
-    stats_ratio['word'] = word
-    stats_ratio['request_id'] = reqId
-    stats_sentence['requests'] = {'request_id': reqId}
+    print(now)
+    ret = stats_ratio
+    ret['word'] = word
+    ret['request_id'] = reqId
+    stats_table = {
+        'analyze_quantity': stats_ratio['total'],
+        'average_feeling': stats_ratio['positive'] - stats_ratio['negative'],
+        'positive_comment': stats_ratio['positive'],
+        'negative_comment': stats_ratio['negative'],
+        'neutral_comment': stats_ratio['neutral'],
+        'created_time': now
+    }
+    stats_ratio['requests'] = {'request_id': reqId}
+    stats_ratio['unanalyzed'] = 0
     log.info('Prediction done. Sending values to the API')
-    req.post('/api/v1/analyzes/', stats_sentence, {'Authorization': 'Bearer '+os.getenv('API_TOKEN')})
-    req.put('/api/v1/requests/'+str(reqId), {"update_time": str(date.today()), "state": "DONE"}, {'Authorization': 'Bearer '+os.getenv('API_TOKEN')})
-    return stats_ratio
+
+    word_id = req.post('/api/v1/word/send', {"word": word.lower()})
+    stats_table['words'] = {'id': word_id.json()['id']}
+    req.post('/api/v1/stats/', stats_table)
+
+    req.post('/api/v1/analyzes/', stats_ratio)
+    req.put('/api/v1/requests/'+str(reqId), {"update_time": now, "state": "DONE"})
+    return ret
 
 
 def runGenerate(path):
